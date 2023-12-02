2f2c2869a0fde370cffe4326e96f5f8bdbd0b0d7
Hayden Muhl <haydenmuhl@gmail.com>
2023-12-01 19:05:41 -0500

    Fix Day 1 solution
    This fixes a bug where overlapping numbers like `eightwo`
    detect "eight" but not "two". Turns out, due to my
    specific input, all the errors cancelled out and I ended
    up with the right answer anyway.

--- src/day01.nim
+++ src/day01.nim
@@ -34,38 +34,28 @@ proc part2() =
       var digit = -1
       if line.startsWith("one"):
         digit = 1
-        line = line[3 .. line.len() - 1]
       elif line.startsWith("two"):
         digit = 2
-        line = line[3 .. line.len() - 1]
       elif line.startsWith("three"):
         digit = 3
-        line = line[5 .. line.len() - 1]
       elif line.startsWith("four"):
         digit = 4
-        line = line[4 .. line.len() - 1]
       elif line.startsWith("five"):
         digit = 5
-        line = line[4 .. line.len() - 1]
       elif line.startsWith("six"):
         digit = 6
-        line = line[3 .. line.len() - 1]
       elif line.startsWith("seven"):
         digit = 7
-        line = line[5 .. line.len() - 1]
       elif line.startsWith("eight"):
         digit = 8
-        line = line[5 .. line.len() - 1]
       elif line.startsWith("nine"):
         digit = 9
-        line = line[4 .. line.len() - 1]
       elif line.startsWith("zero"):
         digit = 0
-        line = line[4 .. line.len() - 1]
       else:
         if line[0] >= '0' and line[0] <= '9':
           digit = line[0].ord() - '0'.ord()
-        line = line[1 .. line.len() - 1]
+      line = line[1 .. line.len() - 1]
 
       if digit != -1:
         if firstDigit == -1:
