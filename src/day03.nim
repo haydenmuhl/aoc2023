90cfc688c6e3fc0f9df82a23eb288df27172c098
Hayden Muhl <haydenmuhl@gmail.com>
2023-12-04 02:15:41 -0500

    Days 3 and 4, 2023

--- src/day03.nim
+++ src/day03.nim
@@ -1,7 +1,138 @@
+import std/re
+import std/strutils
+
+let intRegex = re(r"\d+")
+let nonDigitRegex = re(r"[^\d]")
+
+type coord = object
+  row: int
+  col: int
+
+type engine = object
+  rows: int
+  cols: int
+  data: seq[string]
+
+proc initEngine(data: string): engine =
+  result.data = data.split
+  result.data = result.data[0 ..< result.data.len() - 1]
+  result.rows = result.data.len()
+  result.cols = result.data[0].len()
+
+proc isValid(self: engine, rowIdx: int, bounds: (int, int)): bool =
+  for r in rowIdx - 1 .. rowIdx + 1:
+    if r < 0 or r >= self.data.len():
+      continue
+    let line = self.data[r]
+    let minIdx = max(0, bounds[0] - 1)
+    let maxIdx = min(bounds[1] + 1, line.len() - 1)
+    let subStr = line[minIdx .. maxIdx]
+    echo(subStr)
+    for c in subStr:
+      case c
+      of '0'..'9', '.':
+        continue
+      else:
+        return true
+  false
+
+proc getInt(self: engine, rowIdx: int, bounds: (int, int)): int =
+  self.data[rowIdx][bounds[0] .. bounds[1]].parseInt()
+
+proc part1(self: engine): int =
+  result = 0
+  var rowIdx = 0
+  while rowIdx < self.rows:
+    let line = self.data[rowIdx]
+    var start = 0
+    while start < line.len():
+      let bounds = findBounds(line, intRegex, start)
+      if bounds[0] == -1:
+        break
+      if(self.isValid(rowIdx, bounds)):
+        echo(rowIdx, ' ', bounds[0], ' ', bounds[1])
+        result += self.getInt(rowIdx, bounds)
+      start = bounds[1] + 1
+    rowIdx += 1
+
 proc part1() =
-  discard
+  let infile = open("data/day03.txt")
+  defer: infile.close()
+
+  let engine = initEngine(infile.readAll())
+  echo(engine.part1())
+
+proc getInt(self:engine, rowIdx: int, colIdx: int): int =
+  if rowIdx < 0 or rowIdx >= self.data.len():
+    return -1
+  if colIdx < 0 or colIdx >= self.data.len():
+    return -1
+  if self.data[rowIdx][colIdx] notin Digits:
+    return -1
+
+  result = self.data[rowIdx][colIdx].ord() - '0'.ord()
+
+  # go left
+  var cursor = colIdx - 1
+  var multiplier = 10
+  while cursor >= 0:
+    if self.data[rowIdx][cursor] notin Digits:
+      break
+    result += multiplier * (self.data[rowIdx][cursor].ord() - '0'.ord())
+    multiplier *= 10
+    cursor -= 1
+  # go right
+  cursor = colIdx + 1
+  while cursor < self.data.len():
+    if self.data[rowIdx][cursor] notin Digits:
+      break
+    result *= 10
+    result += self.data[rowIdx][cursor].ord() - '0'.ord()
+    cursor += 1
+
+
+proc adjacentInts(self: engine, rowIdx: int, colIdx: int): seq[int] =
+  var i = self.getInt(rowIdx - 1, colIdx)
+  if i != -1:
+    result.add(i)
+  else:
+    i = self.getInt(rowIdx - 1, colIdx - 1)
+    if i != -1:
+      result.add(i)
+    i = self.getInt(rowIdx - 1, colIdx + 1)
+    if i != -1:
+      result.add(i)
+  i = self.getInt(rowIdx + 1, colIdx)
+  if i != -1:
+    result.add(i)
+  else:
+    i = self.getInt(rowIdx + 1, colIdx - 1)
+    if i != -1:
+      result.add(i)
+    i = self.getInt(rowIdx + 1, colIdx + 1)
+    if i != -1:
+      result.add(i)
+  i = self.getInt(rowIdx, colIdx - 1)
+  if i != -1:
+    result.add(i)
+  i = self.getInt(rowIdx, colIdx + 1)
+  if i != -1:
+    result.add(i)
+
+proc part2(self: engine): int =
+  result = 0
+  for rowIdx in 0 ..< self.rows:
+    for colIdx in 0 ..< self.cols:
+      if self.data[rowIdx][colIdx] == '*':
+        let adjacent = self.adjacentInts(rowIdx, colIdx)
+        if adjacent.len() == 2:
+          result += adjacent[0] * adjacent[1]
 
 proc part2() =
-  discard
+  let infile = open("data/day03.txt")
+  defer: infile.close()
 
-part1()
+  let engine = initEngine(infile.readAll())
+  echo(engine.part2())
+
+part2()
--- src/day04.nim
+++ src/day04.nim
@@ -1,7 +1,46 @@
+import lib
+import std/sets
+import std/math
+import std/tables
+
 proc part1() =
-  discard
+  let infile = open("data/day04.txt")
+  defer: infile.close()
+
+  var total = 0
+  for line in infile.lines():
+    let ints = line.parseInts()
+    let winning = toSet(ints[1..10])
+    let mynums = toSet(ints[11 ..< ints.len()])
+    let common = winning * mynums
+    if common.len() > 0:
+      var score = 1
+      for _ in 0 ..< common.len() - 1:
+        score *= 2
+      total += score
+
+  echo(total)
 
 proc part2() =
-  discard
+  let infile = open("data/day04.txt")
+  defer: infile.close()
 
-part1()
+  var total = 0
+  var cards = initCountTable[int]()
+  for line in infile.lines():
+    let ints = line.parseInts()
+    let id = ints[0]
+    cards.inc(id, 1)
+
+    let winning = toSet(ints[1..10])
+    let mynums = toSet(ints[11 ..< ints.len()])
+    let common = winning * mynums
+    for diff in 1..common.len():
+      cards.inc(id + diff, cards[id])
+    total += cards[id]
+  echo(cards)
+  echo(total)
+
+
+
+part2()
